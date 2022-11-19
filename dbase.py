d884e1b857c7860a33366aad9e9e83ea16029992
danilkyrychenko <danila232005@gmail.com>
2022-11-19 10:57:44 +0200

    change filename

--- dbase.py
+++ dbase.py
@@ -2,10 +2,43 @@ class QuestionDB:
     def __init__(self, db):
         self.db = db
 
-    def set_question(self, question):
+    def add_information(self, question):
         cursor = self.db.cursor()
         cursor.execute(
-            "INSERT INTO question (user_id, question, date) VALUES (?, ?, ?)",
-            (question['user_id'], question['question'], question['date'])
+            "INSERT INTO users (id, user_id, user_name, user_last_name, date,) VALUES (?, ?, ?, ?, ?)",
+            (question['id'], question['user_id'], question['user_name'], question['user_last_name'], question['message_date'])
         )
         self.db.commit()
+
+    def add_question(self, question):
+        cursor = self.db.cursor()
+        cursor.execute(
+            "INSERT INTO question (id, user_id, question, date,) VALUES (?, ?, ?, ?)",
+            (question['id'], question['user_id'], question['question'], question['date'])
+        )
+        self.db.commit()
+
+
+class DataBase:
+    def __init__(self, db):
+        self.db = db
+
+
+def create_tables(db):
+    cursor = db.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                        "id" INTEGER PRIMARY KEY,
+                        "user_id" INTEGER,
+                        "user_name" CHAR(40),
+                        "user_last_name" CHAR(40),
+                        "message_date" TEXT
+                        FOREIGN KEY ("user_id")  REFERENCES question ("user_id")
+                    )''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS question (
+                        "id" INTEGER PRIMARY KEY,
+                        "user_id" INTEGER,
+                        "question" TEXT,
+                        "message_date" TEXT
+                    )''')
+
+    db.commit()
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from telebot import types
 import telebot
+from datetime import datetime
 import sqlite3
 from utils import messages
+from utils import database_dict
+from dbase import create_tables
+from dbase import QuestionDB
+
 
 bot = telebot.TeleBot("5580320258:AAEYAMH3Gg_XZApHEi4hOgzk7uNqmszUDX8")
+connect = sqlite3.connect('telebot.db')
 
 
 @bot.message_handler(commands=['start'])
@@ -22,8 +28,51 @@ def user_registration(message):                                #Connection with 
 def register_question(message):
     if message.text == messages['question_btn_text']:
         bot.send_message(message.from_user.id, messages['get_question'])
+        bot.register_next_step_handler(message, register_information)
 
 
-if name == '__main__':
+def register_information(message):
+    reformat_date = datetime.now().strftime("%d.%m.%G %H:%M:%S")
+    c_date, c_time = reformat_date.split()
+    message_datetime = f"{database_dict['date_message']}: {c_date}\n{database_dict['time_message']}: {c_time}"
+
+
+    # while True:
+    #     if message.text == messages['question_btn_text']:
+    #         bot.send_message(message.from_user.id, messages['get_question'])
+    #         break
+    #     elif message.text != messages['question_btn_text']:
+    #         break
+
+    question_db = QuestionDB(db)
+
+    question = {
+        'id': None,
+        'user_id': message.chat.id,
+        'user_name': message.from_user.first_name,
+        'user_last_name': message.from_user.last_name,
+        'date': message_datetime,
+        'question': message.text
+    }
+    
+    question_db.set
+    print(question['id'])
+    print(message.chat.id)
+    print(message.from_user.first_name)
+    print(message.from_user.last_name)
+    print(message_datetime)
+    print(message.text)
+
+
+
+# @bot.message_handler()
+# def finish_answering:
+#     if
+#
+
+
+
+
+if __name__ == '__main__':
     create_tables(connect)
     bot.polling(none_stop=True)
